ba45dcbb2aaecaffcd4be100fe3b9dbbac632a65
prakash.reddy <prakash.reddy@neosoftmail.com>
2023-03-21 18:34:11 +0530

    ll

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    name: String,
+    username: String,
+    password: String,
+    email_id: String,
+    contact_num: Number,
+    profile_img: String
+},{versionKey: false})
+export const userModel = mongoose.model('user', userSchema, 'user')
+
+console.log('ll')
